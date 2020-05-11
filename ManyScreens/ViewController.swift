276afb59b18e0df7e9605d29fbbc3f16da7b0805
Rafal_O <Rafal_O@imac.local>
2020-05-11 18:00:55 +0200

    Initial Commit

---
+++ ManyScreens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManyScreens
+//
+//  Created by Rafal_O on 11.05.2020.
+//  Copyright © 2020 Rafal_O. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
